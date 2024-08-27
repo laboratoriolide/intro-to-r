a6931fc520ea939c04c0e484c6a8d75b80f928cb
Daniel Sánchez Pazmiño <dsanchezp998@gmail.com>
2024-08-27 00:30:21 -0700

    limpieza

---
+++ spanish/scripts/clases/plan_limpieza_pt2.R
@@ -0,0 +1,9 @@
+# Extras 
+# Bind rows de dos bases de datos
+# Creacion de un catalogo
+# Descarga de base de datos programatica: SUPERCIAS Directorio. https://www.supercias.gob.ec/
+# Carga de datos XLSX con el paquete readxl, edicion preliminar con los argumentos readxl
+# Carga de datos CSV con readcsv, edicion preliminar con los argumentos readcsv, base de datos de SRI banco de datos.
+# Creacion de un catalogo de provincias de acuerdo al estandar DPA (datos INEC geoportal, listados 2024)
+# https://www.ecuadorencifras.gob.ec/documentos/web-inec/Geografia_Estadistica/Micrositio_geoportal/index.html
+# Joins de bases de datos mediante el catalogo
